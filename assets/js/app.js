748e22fd8da4f0e6c11d08096cd35a9c38e21c3a
ANaRobe <ana.maria.robe@gmail.com>
2022-06-13 23:27:36 +0000

    crea the addToDo() w to list the input's text. If there is no text when clicking=> placeholder changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,34 @@ function createNewList() {
     const toDo = document.querySelector("#to-do"); 
     intro.classList.add("fadeOut");
     toDo.classList.add("fadeIn");
-    };
+};
+
+// ADD NEW TASK
+//Select DOM
+let input = document.querySelector("#input");
+let tasks = document.querySelector("#tasks");
+
+function addToDo() {
+    //RESTORE INPUT 
+    input.style.backgroundColor="white";
+    //CREATE LIST  
+    if (input.value.length > 0) { 
+        let todo = document.createElement("li");
+        todo.innerHTML = `
+            <div class="item">${input.value}</div>
+            <div class="itemOptions">
+            <button class="check" type="check"  onclick="checkTodo();"><i class="fas fa-check"></i></button>
+            <button class="delete" type="trash" onclick="deleteTodo();"><i class="fas fa-trash"></i></button>
+            <div>`;      
+        todo.classList.add("todo");
+        tasks.appendChild(todo);
+    }else{
+    //ALERT WHEN NO TEXT  
+    input.style.backgroundColor="pink";
+    input.placeholder="Don't forget to type in a task!";
+  };
+}
+
+//CHECK TODO
+
+//DELETE TODO
