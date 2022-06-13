65dd88c74c5d82281aa74531547eb81aaf92f048
ANaRobe <ana.maria.robe@gmail.com>
2022-06-13 05:38:11 +0000

    Add ctionality to #create btn

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+//START NEW TO DO LIST
+function createNewList() {
+    const intro = document.querySelector("#intro");
+    const toDo = document.querySelector("#to-do"); 
+    intro.classList.add("fadeOut");
+    toDo.classList.add("fadeIn");
+    };
